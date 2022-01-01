4fbcc2084b58558664680ec93d0900c3f630f071
sebastianbk98 <sebastianbk007@gmail.com>
2022-01-01 21:45:56 +0700

    Add class Penjualan and Pembelian

---
+++ Sistem_Minimarket/src/main/java/com/unsada/pbo/util/Penjualan.java
@@ -0,0 +1,16 @@
+package com.unsada.pbo.util;
+
+public class Penjualan {
+    private int jumlah_produk;
+    private double total_harga;
+
+    public Penjualan(int jumlah_produk, double total_harga) {
+        this.jumlah_produk = jumlah_produk;
+        this.total_harga = total_harga;
+    }
+    
+    public void getPenjualanByPembelian(){
+        
+    }
+    
+}
